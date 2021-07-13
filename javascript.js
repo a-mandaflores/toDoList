0d8b3c1c8e59159bb2646d7ccc0c2b794b0f74a4
Amanda <nanda.hamandadesouza@gmail.com>
2021-07-13 11:29:44 -0300

    Atualização JS

--- javascript.js
+++ javascript.js
@@ -4,12 +4,43 @@
             <input type="button" value="X" class="x">
         </label> */}
 
-        const criarItem = () => {
+        let banco = [
+            {'tarefa':'Estudar JS', 'status': ''},
+            {'tarefa': 'netflix', 'status': 'checked'},
+            
+        ]
+
+        const criarItem = (text, status) => {
             const item = document.createElement('label')
             item.classList.add('checkbox')
             item.innerHTML = `
-                <input id="check" type="checkbox" class="checkbox-bt">
-                <div class="checkbox-text">Teste de item</div>
-                <input type="button" value="X" class="x">
+                <input id="check" type="checkbox" class="checkbox-bt" ${status}>
+                <div class="checkbox-text">${text}</div>
+                <input type="button" value="x" class="x">
             `
+            document.getElementById('todoList').appendChild(item)
         }
+
+        const limparTarefas = () => {
+            const todoList = documento.getElementById('todoList')
+            while (todoList.firstChild) {
+                todoList.removeChild(todoList.lastCrhild)
+            }
+        }
+
+        const atualizarTela = () => {
+            limparTarefas
+            banco.forEach (item => criarItem(item.tarefa));
+        }
+
+        const inserirItem= (evento) =>{
+            const techa = evento.key
+            if (tecka === 'Enter') {
+                banco.push ({'tarefa':'Estudar JS', 'status': ''})
+                atualizarTela();
+            }
+        }
+        document.getElementById('newItem').addEventListener('keypress', inserirItem)
+
+        atualizarTela();
+
