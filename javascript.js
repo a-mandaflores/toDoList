56b6895336f2dfee8f6e5fb75c4d4aa7fdfa6401
Amanda <nanda.hamandadesouza@gmail.com>
2021-07-13 21:14:14 -0300

    Atualizações do JS

--- javascript.js
+++ javascript.js
@@ -6,41 +6,55 @@
 
         let banco = [
             {'tarefa':'Estudar JS', 'status': ''},
-            {'tarefa': 'netflix', 'status': 'checked'},
-            
-        ]
+            {'tarefa': 'netflix', 'status': 'checked'}            
+        ];
 
-        const criarItem = (text, status) => {
+        const criarItem = (tarefa, status, indice) => {
             const item = document.createElement('label')
             item.classList.add('checkbox')
             item.innerHTML = `
-                <input id="check" type="checkbox" class="checkbox-bt" ${status}>
-                <div class="checkbox-text">${text}</div>
-                <input type="button" value="x" class="x">
+                <input id="check" type="checkbox" class="checkbox-bt" ${status} data-indice=${indice}>
+                <div class="checkbox-text">${tarefa}</div>
+                <input type="button" value="x" class="x" data-indice=${indice}>
             `
-            document.getElementById('todoList').appendChild(item)
+            document.getElementById('toDoList').appendChild(item)
         }
 
         const limparTarefas = () => {
-            const todoList = documento.getElementById('todoList')
-            while (todoList.firstChild) {
-                todoList.removeChild(todoList.lastCrhild)
+            const toDoList = document.getElementById('toDoList');
+            while (toDoList.firstChild){
+                toDoList.removeChild(toDoList.lastChild);
             }
         }
 
-        const atualizarTela = () => {
-            limparTarefas
-            banco.forEach (item => criarItem(item.tarefa));
-        }
+         const atualizarTela = () => {
+             limparTarefas();
+             banco.forEach ( (item, indice) => criarItem(item.tarefa, item.status, indice))
+         }
 
-        const inserirItem= (evento) =>{
-            const techa = evento.key
-            if (tecka === 'Enter') {
-                banco.push ({'tarefa':'Estudar JS', 'status': ''})
+         const inserirItem = (evento) =>{
+            const tecla = evento.key
+            const tarefa = evento.target.value
+            if (tecla === 'Enter') {
+                banco.push ({'tarefa': tarefa, 'status': ''});
                 atualizarTela();
+                evento.target.value = ''
             }
         }
-        document.getElementById('newItem').addEventListener('keypress', inserirItem)
-
+        const removerItem = (indice) => {
+            banco.splice (indice,1);
+            atualizarTela();
+        }
+        const clickItem = (evento) =>{
+            const elemento = evento.target
+            if (elemento.type === 'button'){
+                const indice = elemento.dataset.indice
+                removerItem(indice)
+            }
+        }
+        document.getElementById('newItem').addEventListener('keypress', inserirItem) 
+        document.getElementById('toDoList').addEventListener('click', clickItem)
+ 
         atualizarTela();
+        
 
